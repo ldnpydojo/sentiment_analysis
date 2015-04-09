752edcf5a25a4eca0954d79671c1128c37895e89
Dean Elbaz <elbaz.dean@gmail.com>
2015-04-09 21:17:58 +0100

    like

--- sentiment.py
+++ sentiment.py
@@ -20,7 +20,14 @@ def newinput(source, tuple_list):
         print "%s: %f" % (title, score)
     print "Average score: %f" % avg_score
 
+    if avg_score > 0:
+        print "%s likes %s" % (source, search_term)
+    else:
+        print "%s dislikes %s" % (source, search_term)
+
+
 if __name__ == '__main__':
+    search_term = "David Cameron"
     examples = [
         ("http://example.com/1", "Article 1", "This is very good"),
         ("http://example.com/2", "Article 2", "This is very bad"),
