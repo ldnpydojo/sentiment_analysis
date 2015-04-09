50ca4d815c439d0e1aad5cdf20ebe457eb07cb1a
Dean Elbaz <elbaz.dean@gmail.com>
2015-04-09 21:05:27 +0100

    tuples

--- sentiment.py
+++ sentiment.py
@@ -4,4 +4,14 @@ from textblob import TextBlob
 def get_polarity(text):
     return TextBlob(text).sentiment.polarity
 
-print get_polarity("hello!")
+
+# (url, title, text)
+def newinput(source, tuple_list):
+    result = []
+    score = 0
+    for url, title, text in tuple_list:
+        polarity = get_polarity(text)
+        score += polarity
+        result.append((url, title, polarity))
+    avg = score / len(tuple_list)
+    return result, avg
